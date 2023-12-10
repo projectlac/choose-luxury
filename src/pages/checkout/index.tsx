6baa9d9fe604ef7c13d64d853a6988caacc686f1
projectlac <dft1711198@gmail.com>
2023-12-10 16:06:34 +0700

    Change

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -2,7 +2,6 @@ import { styled } from '@mui/material/styles';
 import CheckoutIndex from 'components/checkout/CheckoutIndex';
 import Footer from 'components/landingpage/Footer';
 import Header from 'components/landingpage/Header';
-import RelatedProduct from 'components/product/RelatedProduct';
 import Breadcrumbs from 'components/widget/Breadcum';
 import AppBar from 'ui-component/extended/AppBar';
 const HeaderWrapper = styled('div')(({ theme }) => ({
@@ -19,7 +18,7 @@ function ProductDetailIndex() {
       </HeaderWrapper>
       <Breadcrumbs />
       <CheckoutIndex />
-      <RelatedProduct />
+      {/* <RelatedProduct id={}/> */}
       <Footer />
     </div>
   );
