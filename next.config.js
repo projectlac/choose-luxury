f71dc2c4522a9924d4dddf55a6b8b62183c95d6a
projectlac <dft1711198@gmail.com>
2023-12-19 23:04:13 +0700

    Update new domain image

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const withTM = require('next-transpile-modules')([
 const nextConfig = withTM({
   reactStrictMode: true,
   images: {
-    domains: ['pos.nvncdn.net', 'clux.azurewebsites.net', 'cluxstorage.blob.core.windows.net']
+    domains: ['pos.nvncdn.net', 'clux.azurewebsites.net', 'cluxstorage.blob.core.windows.net', 'chooselux.blob.core.windows.net']
   }
 });
 
