fbc8115bc9ec90a07d2f5e260a240b87fc3c3921
projectlac <dft1711198@gmail.com>
2024-01-08 23:51:49 +0700

    Add response

--- src/components/landingpage/NewProduct.tsx
+++ src/components/landingpage/NewProduct.tsx
@@ -31,7 +31,32 @@ const NewProduct = () => {
     swipeToSlide: true,
     focusOnSelect: true,
     centerPadding: '15px',
-    slidesToShow: noItems
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
   const getListProduct = useCallback(async () => {
